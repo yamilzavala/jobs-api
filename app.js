004c4dd221de5193303ea5b20bfbf8dd79b7242c
yamilzavala <zavalayamil9@gmail.com>
2024-12-23 15:26:44 -0300

    cors position updated

--- app.js
+++ app.js
@@ -34,15 +34,15 @@ const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml')
 
-app.use(express.json());
-app.use(helmet())
-app.use(cors())
-app.use(xss())
-
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth',authRoutes)
