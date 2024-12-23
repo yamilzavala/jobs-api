2e53a36123a801830df017d4218830ab9b6fe7b2
yamilzavala <zavalayamil9@gmail.com>
2024-12-23 13:29:12 -0300

    swagger added

--- app.js
+++ app.js
@@ -28,14 +28,22 @@ app.use(rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 100 //Limit each IP to 100 requests per `window`
 }))
+
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
 
 app.get('/', (req, res) => {
-  res.send('jobs api')
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 // routes
 app.use('/api/v1/auth',authRoutes)
 app.use('/api/v1/jobs', authenticateUser, jobsRoutes)
